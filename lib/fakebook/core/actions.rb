b79ffe64c267f29c795697dd3feb8bc7e22805fe
Arthur Granado <agranado@pluga.co>
2017-07-22 14:21:22 -0300

    Refacto Actions - send_money

--- lib/fakebook/core/actions.rb
+++ lib/fakebook/core/actions.rb
@@ -2,16 +2,17 @@ module Fakebook
   module Core
     class Actions
       def send_money(user_a, user_b, value, api)
-        api_response = api.create_payment(user_b.username, value)
-        api_payment_id = api_response[:success][:external_payment_id]
-        api_recipient_id = api_response[:success][:external_recipient_id]
-        payment_args = {value: value,
-                        recipient: user_b.username,
-                        external_payment_id: api_payment_id,
-                        external_recipient_id: api_recipient_id}
-        payment = Payment.new(payment_args)
-        user_a.add_payment(payment)
+        user_a.add_payment(Payment.new(create_payment_params(value, user_b, api.create_payment(user_b.username, value))))
         user_a
+      end
+
+      def create_payment_params(value, user, api_response)
+        {
+          value: value,
+          recipient: user.username,
+          external_payment_id: api_response[:success][:external_payment_id],
+          external_recipient_id: api_response[:success][:external_recipient_id]
+        }
       end
 
       def list_payments(user, api)
