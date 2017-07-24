32895137af4c34a79748d94a61972c4aabdd936f
Arthur Granado <agranado@pluga.co>
2017-07-24 15:38:10 -0300

    Remove Payment and DumpDB that are not useful any more

--- lib/fakebook.rb
+++ lib/fakebook.rb
@@ -15,7 +15,5 @@ class Hash
   end
 end
 require_relative "./fakebook/core/user"
-require_relative "./fakebook/core/payment"
 require_relative "./fakebook/core/actions"
 require_relative "./fakebook/gateway/cool_pay"
-require_relative "./fakebook/core/dump_db"
