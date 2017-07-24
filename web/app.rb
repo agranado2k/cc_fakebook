9e6140968a2efc6bc966b1eda3d2b946185f3c33
Arthur Granado <agranado@pluga.co>
2017-07-24 20:49:12 -0300

    Web - List payments from current user

--- web/app.rb
+++ web/app.rb
@@ -1,5 +1,10 @@
 require "sinatra"
+require_relative "../lib/fakebook"
 
 get "/" do
+  @current_user = Fakebook::Core::User.new({username: "User A", credentials: {username: "arthur", apikey: "68830AEF4DBFAD18"}})
+  cool_pay = Fakebook::Gateway::CoolPay.new(@current_user.credentials)
+  @payments = cool_pay.list_payments
+
   erb :index
 end
